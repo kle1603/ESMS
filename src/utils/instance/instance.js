72e4c7c41af8dba29bcd8b010e931f8541805b5a
kle1603 <bkhang160303@gmail.com>
2023-11-08 09:05:21 +0700

    .

--- src/utils/instance/instance.js
+++ src/utils/instance/instance.js
@@ -1,15 +1,18 @@
 import configs from "@/configs";
 import axios from "axios";
-import cookies from "../cookies";
+// import cookies from "../cookies";
 
-const token = cookies.getToken();
+// const token = cookies.getToken();
 
 const instance = axios.create({
     baseURL: configs.publicRuntime.API_URL,
     // timeout: 1000,
     headers: {
-        Authorization: `Bearer ${token}`,
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
     },
+    credentials: "include",
+    withCredentials: true,
 });
 
 export default instance;
